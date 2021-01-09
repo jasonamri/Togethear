b791ec01181343375ea45b20235ce3935ad6ad7a
Jason Amri <jasonamri@gmail.com>
2021-01-09 18:15:20 -0500

    Fixed client_secret

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 
 var client_id = process.env.CLIENT_ID; // Your client id
-var client_secret = process.env.CLIENT_ID; // Your secret
+var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = 'https://togethear.jasonamri.com/callback'; // Your redirect uri
 var stateKey = 'spotify_auth_state';
 
