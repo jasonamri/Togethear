d155179e71077b7ab70eb7b5829480ae91c499e9
Jason Amri <jasonamri@gmail.com>
2021-01-09 20:31:43 -0500

    Moved /login to component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { DoneComponent } from './done/done.component';
 import { GoComponent } from './go/go.component';
 import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'go', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'go', component: GoComponent },
-  { path: 'done', component: DoneComponent }
+  { path: 'done', component: DoneComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
